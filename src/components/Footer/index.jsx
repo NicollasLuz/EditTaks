37e30fbb5ce2c9ade987abb56174bb2e83566fc5
Nicollas Luz <nicollasgoias@hotmail.com>
2024-11-14 22:03:41 -0300

    criando footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Feito com 🧡 por Nicollas Luz © 2024
+        </footer>
+    )
+}
+
+export default Footer
